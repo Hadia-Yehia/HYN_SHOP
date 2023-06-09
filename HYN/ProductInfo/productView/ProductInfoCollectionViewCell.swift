3425c1cd832fd8df2b0a9b8fe203f4a94c16c550
Hadia yehia <hadiayzmm@gmail.com>
2023-06-09 20:04:06 +0300

    product info new

---
+++ HYN/ProductInfo/productView/ProductInfoCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductInfoCollectionViewCell.swift
+//  HYN
+//
+//  Created by Hadia Yehia on 06/06/2023.
+//
+
+import UIKit
+
+class ProductInfoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HYN/ProductInfo/productView/ReviewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewTableViewCell.swift
+//  HYN
+//
+//  Created by Hadia Yehia on 07/06/2023.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
