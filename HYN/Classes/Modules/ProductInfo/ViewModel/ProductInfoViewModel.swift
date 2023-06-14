7657bd774c9c2bcfc686f66d328761ad395a336a
Hadia yehia <hadiayzmm@gmail.com>
2023-06-14 23:33:40 +0300

    favorite merge

--- HYN/Classes/Modules/ProductInfo/ViewModel/ProductInfoViewModel.swift
+++ HYN/Classes/Modules/ProductInfo/ViewModel/ProductInfoViewModel.swift
@@ -98,7 +98,7 @@ class ProductInfoViewModel{
     }
     
     func saveItemToDatabase(){
-        let item = Fav(title: product.name, price: checkCurrency(), img: product.imgs.first ?? "placeholder", id: productId)
+        let item = Fav(title: product.name, price: newCurrency ?? "no data", img: product.imgs.first ?? "placeholder", id: productId)
         FavCoreData.saveItemToDataBase(favItem: item)
     }
 
