0cebfd29322ce58b2e5c363cab354e6ca105a718
nada youssef <nadayousif99.ny.ny@gmail.com>
2023-06-18 18:25:27 -0700

    order view model

--- HYN/Classes/Modules/Orders/Screen/OrderViewController.swift
+++ HYN/Classes/Modules/Orders/Screen/OrderViewController.swift
@@ -12,25 +12,45 @@ class OrderViewController: UIViewController ,UITableViewDataSource, UITableViewD
     
 
     @IBOutlet weak var orderTable: UITableView!
+    var viewModel = OrdersViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         orderTable.register(UINib(nibName: "OrderTableViewCell", bundle: nil), forCellReuseIdentifier: "cellOrder")
         orderTable.dataSource = self
         orderTable.delegate = self
+        self.bindViewModel()
+        viewModel.getOrderData()
         // Do any additional setup after loading the view.
+    }
+    func bindViewModel(){
+        viewModel.isLoading.bind{[weak self] isLoading in
+            guard let self = self , let isLoading = isLoading
+            else{return}
+            DispatchQueue.main.async {
+                if isLoading{
+                    print("loadingggg")
+                }else{
+                    print("no loadinggg")
+                    self.orderTable.reloadData()
+                }
+            }
+        }
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModel.getCellCount()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellOrder", for: indexPath) as! OrderTableViewCell
-        cell.orderName.text = "shose"
+        print("orders",viewModel.getCellId(index: indexPath.row))
+        cell.configCell(id: viewModel.getCellId(index: indexPath.row), price: viewModel.getCellPrice(index: indexPath.row), date: viewModel.getCellDate(index: indexPath.row))
+        /*cell.orderName.text = "shose"
         cell.priceLabel.text = "800"
-        cell.dataLabel.text = "8/9/2023"
+        cell.dataLabel.text = "8/9/2023"*/
         return cell
     }
     /*
