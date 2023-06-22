5a096983ecc78e932700e1471759edd4b3463474
YousraMamdouh <yousramamdouh98@gmail.com>
2023-06-22 02:56:56 -0700

    - in this commit: - cleaning MockableNetworkService class

--- HYNTests/Mocking/MockNetworkCaller.swift
+++ HYNTests/Mocking/MockNetworkCaller.swift
@@ -22,33 +22,12 @@ class MockNetworkCaller : Mockable {
          case responseError
      }
 
-//     func load<T: Decodable>(_ filename: String)-> T {
-//         var data: Data
-//         guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-//                 else
-//         {
-//              fatalError("Can't open file called \(filename)")
-//         }
-//         do{
-//             data = try Data(contentsOf: file)
-//         }catch
-//         {
-//             fatalError("Can't open file called \(filename)")
-//         }
-//         do{
-//            return try JSONDecoder().decode(T.self, from: data)
-//         }
-//         catch
-//         {
-//             fatalError("Can't open file called \(filename)")
-//         }
-//     }
 
 }
 extension MockNetworkCaller
 {
     
-    // MARK: get all addresses
+    // MARK: get currency exchange
     func getCurrencyExchange(url:String , complitionHandler: @escaping (Currency?,Error?)->Void)
     {
         if shouldReturnError
@@ -57,7 +36,7 @@ extension MockNetworkCaller
         }else
         {
             do {
-                let currency:Currency = try loadJSON(filename: "Leagues", type: Currency.self)
+                let currency:Currency = try loadJSON(filename: "CurrencyResponse", type: Currency.self)
                 complitionHandler(currency,nil)
             }catch
             {
@@ -66,102 +45,5 @@ extension MockNetworkCaller
     
         }
     }
-    
-   // MARK: get upcomingEvents : Basketball
-//    func getUpcomingEvents(url:String , complitionHandler: @escaping ([Event]?,Error?)->Void)
-//    {
-//        if shouldReturnError
-//        {
-//            complitionHandler(nil,ResponseWithError.responseError)
-//        }else
-//        {
-//            do {
-//                let upComingEvents:Events = try loadJSON(filename: "UpComingEvents", type: Events.self)
-//                complitionHandler(upComingEvents.result,nil)
-//            }catch
-//            {
-//                print("Error")
-//            }
-//
-//        }
-//    }
-//
-//    // MARK: get latest events : cricket
-//
-//    func getLatestEvents(url:String , complitionHandler: @escaping ([Event]?,Error?)->Void)
-//    {
-//        if shouldReturnError
-//        {
-//            complitionHandler(nil,ResponseWithError.responseError)
-//        }else
-//        {
-//            do {
-//                let latestEvents:Events = try loadJSON(filename: "LatestEvents", type: Events.self)
-//                complitionHandler(latestEvents.result,nil)
-//            }catch
-//            {
-//                print("Error")
-//            }
-//
-//        }
-//    }
-//
-//    //MARK: get teams : football
-//    func getTeams(url:String , complitionHandler: @escaping ([Team]?,Error?)->Void)
-//    {
-//        if shouldReturnError
-//        {
-//            complitionHandler(nil,ResponseWithError.responseError)
-//        }else
-//        {
-//            do {
-//                let teams:Teams = try loadJSON(filename: "Teams", type: Teams.self)
-//                complitionHandler(teams.result,nil)
-//            }catch
-//            {
-//                print("Error")
-//            }
-//
-//        }
-//    }
-//
-//
-//    // MARK: get footballTeamDetails
-//    func getTeamDetails(url:String , complitionHandler: @escaping ([TeamDetails]?,Error?)->Void)
-//    {
-//        if shouldReturnError
-//        {
-//            complitionHandler(nil,ResponseWithError.responseError)
-//        }else
-//        {
-//            do {
-//                let teams:FootballTeam = try loadJSON(filename: "TeamDetails", type: FootballTeam.self)
-//                complitionHandler(teams.result,nil)
-//            }catch
-//            {
-//                print("Error")
-//            }
-//
-//        }
-//    }
-//
-//    // MARK: get tennis players
-//
-//    func getTennisPlayers(url:String , complitionHandler: @escaping ([TennisPlayer]?,Error?)->Void)
-//    {
-//        if shouldReturnError
-//        {
-//            complitionHandler(nil,ResponseWithError.responseError)
-//        }else
-//        {
-//            do {
-//                let players:TennisPlayers = try loadJSON(filename: "TeamDetails", type: TennisPlayers.self)
-//                complitionHandler(players.result,nil)
-//            }catch
-//            {
-//                print("Error")
-//            }
-//
-//        }
-//    }
+
 }
